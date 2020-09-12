228cd0e3e362892db4ed40c6eba85cc52483cc1e
Metaras <metaras.c@ku.th>
2020-09-12 21:41:56 +0700

    part 7 finished

--- polls/views.py
+++ polls/views.py
@@ -39,7 +39,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
