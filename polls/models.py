7fd63bcf0af7f928aa4099074cf3b9f0c9e76377
Metaras <metaras.c@ku.th>
2020-09-12 19:25:34 +0700

    part 6 finished

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    question_text = "What is your favorite music type?"
     def __str__(self):
         return self.question_text
 
